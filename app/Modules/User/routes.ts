11fe495c17e3d9a9f5d8ecf0599dba664e9a27ab
Yoha Braga <braga.yoha@gmail.com>
2022-04-20 16:49:57 -0300

    feat: defined privates and publics routes

---
+++ app/Modules/User/routes.ts
@@ -0,0 +1,9 @@
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.group(() => {
+  Route.post('/users', 'UsersController.store')
+  Route.get('/users', 'UsersController.index')
+  Route.get('/users/:id', 'UsersController.show')
+  Route.patch('/users/:id', 'UsersController.update')
+  Route.delete('/users/:id', 'UsersController.destroy')
+}).middleware('auth')
--- start/routes.ts
+++ start/routes.ts
@@ -1,30 +1,14 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer'
-|
-*/
-
 import Route from '@ioc:Adonis/Core/Route'
+import 'App/Modules/User/routes'
 
-Route.post('/users', 'UsersController.store')
-Route.get('/users', 'UsersController.index')
-Route.get('/users/:id', 'UsersController.show')
-Route.put('/users/:id', 'UsersController.update')
-Route.delete('/users/:id', 'UsersController.destroy')
+Route.where('id', {
+  match: /^[0-9]+$/,
+  cast: (id) => Number(id),
+})
+
+Route.get('/', () => {
+  return 'Welcome to ours Vehicle Store!'
+})
 
 Route.post('/login', 'AuthController.login')
 Route.post('/logout', 'AuthController.logout')
